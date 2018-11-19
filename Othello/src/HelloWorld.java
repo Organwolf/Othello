2e17ec9efee0849101a8b347c524e3a1c48c1fe0
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2018-11-19 11:48:25 +0100

    Looping Aron

--- Othello/src/HelloWorld.java
+++ Othello/src/HelloWorld.java
@@ -3,6 +3,9 @@
 public class HelloWorld {	
 	public HelloWorld() {
 		System.out.println("Hello world");
+		for(int i=0; i<"aron".length();i++) {
+			System.out.println("aron".charAt(i));
+		}
 	}
 
 	public static void main(String[] args) {
