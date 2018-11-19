f8115d6da5fababc362ea5f627f64de333fa0245
Aron Polner <aron.polner@gmail.com>
2018-11-19 11:35:33 +0100

    Initial commit baby

---
+++ Othello/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+
+public class HelloWorld {	
+	public HelloWorld() {
+		System.out.println("Hello world");
+	}
+
+	public static void main(String[] args) {
+		HelloWorld test = new HelloWorld();
+	}
+}
+
