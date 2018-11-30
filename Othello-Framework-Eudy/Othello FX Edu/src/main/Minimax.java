83562dd45a6a88bf405009e8841ba67e2102fd49
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2018-11-30 14:11:31 +0100

    cleanup

--- Othello-Framework-Eudy/Othello FX Edu/src/main/Minimax.java
+++ Othello-Framework-Eudy/Othello FX Edu/src/main/Minimax.java
@@ -13,6 +13,7 @@ import com.eudycontreras.othello.enumerations.PlayerTurn;
 import com.eudycontreras.othello.models.GameBoardState;
 import com.eudycontreras.othello.threading.ThreadManager;
 import com.eudycontreras.othello.threading.TimeSpan;
+import com.eudycontreras.othello.utilities.GameTreeUtility;
 
 public class Minimax extends Agent {
 	
@@ -27,8 +28,59 @@ public class Minimax extends Agent {
 		
 		ThreadManager.pause(TimeSpan.millis(waitTime)); // Pauses execution for the wait time to cause delay
 		
-		// never use depth = 1 -> will jump to evaluation immedietly
-		int depth = 5;
-		return AgentController.minimaxRoot(depth, gameState, true, playerTurn); 
+		// never use depth = 1 -> will jump to evaluation immediately
+		int depth = 4;
+		return minimaxRoot(depth, gameState, true, playerTurn); 
+	}
+	
+	private MoveWrapper minimaxRoot(int depth, GameBoardState state, boolean isMaximizingPlayer, PlayerTurn playerTurn) {
+			
+			List<ObjectiveWrapper> AvailableMoves = AgentController.getAvailableMoves(state, playerTurn);
+			int bestMove = Integer.MIN_VALUE;
+			MoveWrapper bestMoveFound = new MoveWrapper(null);
+			
+			for (ObjectiveWrapper move : AvailableMoves) {
+				GameBoardState childState = AgentController.getNewState(state, move);	
+				int value = minimax(depth-1, childState, !isMaximizingPlayer, playerTurn);
+				if (value>bestMove) {
+					bestMove = value;
+					bestMoveFound = new MoveWrapper(move);
+				}
+			}	
+			System.out.println("bestMove: "+String.valueOf(bestMove));
+			return bestMoveFound;
+		}
+	
+	private int minimax(int depth, GameBoardState state, boolean isMaximizingPlayer, PlayerTurn playerTurn) {
+		List<ObjectiveWrapper> availableMoves = AgentController.getAvailableMoves(state, playerTurn);
+		// Do we have to terminate if the player can't make any moves?
+		int nbrOfWhiteMoves =  AgentController.getAvailableMoves(state, PlayerTurn.PLAYER_ONE).size();
+		int nbrOfBlackMoves =  AgentController.getAvailableMoves(state, PlayerTurn.PLAYER_TWO).size();
+		if (depth <= 0 || state.isTerminal() || ((nbrOfBlackMoves+nbrOfWhiteMoves)==0)) {
+			//return nbrOfWhiteMoves - nbrOfBlackMoves;
+			return (int) AgentController.getGameEvaluation(state, playerTurn);
+		}
+
+		if (availableMoves.isEmpty()) {
+			//System.out.println("no children, pass to next!");
+			return minimax(depth-1, state, !isMaximizingPlayer, GameTreeUtility.getCounterPlayer(playerTurn));
+		}
+		
+		if (isMaximizingPlayer) {
+			int maxVal = Integer.MIN_VALUE;
+			for (ObjectiveWrapper move : availableMoves) {
+				GameBoardState childState = AgentController.getNewState(state, move);	
+				maxVal = Math.max(maxVal, minimax(depth-1, childState, !isMaximizingPlayer, GameTreeUtility.getCounterPlayer(playerTurn)));
+			}
+			return maxVal;			
+		}		
+		else {
+			int minVal = Integer.MAX_VALUE;
+			for (ObjectiveWrapper move : availableMoves) {
+				GameBoardState childState = AgentController.getNewState(state, move);	
+				minVal = Math.min(minVal, minimax(depth-1, childState, !isMaximizingPlayer, GameTreeUtility.getCounterPlayer(playerTurn)));
+			}
+			return minVal;
+		}		
 	}
 }
